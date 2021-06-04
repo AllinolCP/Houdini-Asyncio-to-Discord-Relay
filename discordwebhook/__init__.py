e6f2af8a42519d06e25c09bd88a536e8daa10423
AllinolCP <56921534+AllinolCP@users.noreply.github.com>
2021-06-04 09:05:02 +0700

    please use the updated version
    https://github.com/workaddiction/houdini-webhooks

--- discordwebhook/__init__.py
+++ discordwebhook/__init__.py
@@ -1,79 +1,71 @@
 from houdini.plugins import IPlugin
 from houdini import handlers
-from discord_webhook import DiscordWebhook, DiscordEmbed
+from dhooks import Webhook, Embed
 from houdini.handlers import XTPacket
-from houdini.handlers.games import determine_coins_earned, game_over_cooling
-import difflib
-import time
+from houdini.handlers.play.message import handle_send_message
+from houdini.handlers.play.navigation import handle_join_server
+
+# Fill these in.
 chatlogwebhook = ''
 coinslogwebhook = ''
 connectionlogwebhook = ''
-cppsname =''
-playlink = '' 
+cppsname = ''
+playlink = ''
 website = ''
 
-
 class DiscordWebhookLogger(IPlugin):
-    author = "Allinol"
-    description = "Discord Webhook plugin"
-    version = "1.0.0"
+    author = "Allinol & atlogical"
+    description = "Discord Webhook Plugin"
+    version = "1.1.0"
 
     def __init__(self, server):
         super().__init__(server)
 
-        self.items_by_name = None
-
     async def ready(self):
-        self.server.logger.info("Discord Webhook Logger Ready!")
-        
-        
-    @handlers.handler(XTPacket('j', 'js'), pre_login=True)   
+        self.server.logger.info("DiscordWebhookLogger.ready()")
+
+    @handlers.handler(XTPacket('j', 'js'), after=handle_join_server)
     async def connected_logger(self, p):
-        webhook = DiscordWebhook(url=connectionlogwebhook, content=f'{p.username} connected to {p.server.config.name}')
-        response = webhook.execute()
-    
+        async with Webhook.Async(connectionlogwebhook) as webhook:
+            embed = Embed(title='Log', description='Connected', color=242424, timestamp='now')
+
+            embed.set_author(name=f'{p.username}', url=f'https://{website}', icon_url=f'https://{playlink}/avatar/{p.id}/cp?size=120')
+            embed.set_thumbnail(url=f'https://{playlink}/avatar/{p.id}/cp?size=120')
+            embed.set_footer(text=f'© {cppsname}')
+            embed.add_field(name='Server', value=f'{self.server.config.name}')
+            embed.add_field(name='Location', value=f'{p.room.name}')
+            embed.add_field(name='Name', value=f'{p.username}')
+            embed.add_field(name=f'Players In {self.server.config.name}', value=f"{await p.server.redis.hget('houdini.population',f'{self.server.config.id}', encoding='utf-8')}")
+
+            await webhook.send(embed=embed)
+
     @handlers.disconnected
     @handlers.player_attribute(joined_world=True)
     async def disconnected(self, p):
-        webhook = DiscordWebhook(url=connectionlogwebhook, content=f'{p.username} disconnected')
-        response = webhook.execute()
-    
-    @handlers.handler(XTPacket('zo', ext='z'))
-    @handlers.cooldown(10, callback=game_over_cooling)
-    async def coins_logger(self, p, score: int):
-        coins_earned = determine_coins_earned(p, score)
-        webhook = DiscordWebhook(url=coinslogwebhook, content=f'{p.username} earned {coins_earned}')
-        response = webhook.execute()
-        
-    @handlers.handler(XTPacket('m', 'sm'))   
+        async with Webhook.Async(connectionlogwebhook) as webhook:
+            embed = Embed(title='Log', description='Disconnected', color=242424, timestamp='now')
+
+            embed.set_author(name=f'{p.username}', url=f'https://{website}', icon_url=f'https://{playlink}/avatar/{p.id}/cp?size=120')
+            embed.set_thumbnail(url=f'https://{playlink}/avatar/{p.id}/cp?size=120')
+            embed.set_footer(text=f'© {cppsname}')
+            embed.add_field(name='Server', value=f'{self.server.config.name}')
+            embed.add_field(name='Name', value=f'{p.username}')
+            embed.add_field(name=f'Players In {self.server.config.name}', value=f"{await p.server.redis.hget('houdini.population',f'{self.server.config.id}', encoding='utf-8')}")
+
+            await webhook.send(embed=embed)
+
+    @handlers.handler(XTPacket('m', 'sm'), after=handle_send_message)
+    @handlers.cooldown(1)
     async def chat_logger(self, p, penguin_id: int, message: str):
-        webhook = DiscordWebhook(url=chatlogwebhook)
+        async with Webhook.Async(chatlogwebhook) as webhook:
+            embed = Embed(title='Chat-Log', description='Chat logger', color=242424, timestamp='now')
 
-        # create embed object for webhook
-        embed = DiscordEmbed(title='Chat-Log', description='Chat logger', color=242424)
+            embed.set_author(name=f'{p.username}', url=f'https://{website}', icon_url=f'https://{playlink}/avatar/{penguin_id}/cp?size=120')
+            embed.set_thumbnail(url=f'https://{playlink}/avatar/{penguin_id}/cp?size=120')
+            embed.set_footer(text=f'© {cppsname}')
+            embed.add_field(name='Server', value=f'{self.server.config.name}')
+            embed.add_field(name='Location', value=f'{p.room.name}')
+            embed.add_field(name='Name', value=f'{p.username}')
+            embed.add_field(name='Message', value=f'{message}')
 
-        # set author
-        embed.set_author(name=f'{p.username}', url=f'https://{website}', icon_url=f'https://{playlink}/avatar/{penguin_id}/cp?size=120')
-
-
-        # set thumbnail
-        embed.set_thumbnail(url=f'https://{playlink}/avatar/{penguin_id}/cp?size=120')
-
-        # set footer
-        embed.set_footer(text=f'© {cppsname}')
-
-        # set timestamp (default is now)
-        embed.set_timestamp()
-
-        # add fields to embed
-        embed.add_embed_field(name='Server', value=f'{self.server.config.name}')
-        embed.add_embed_field(name='Location', value=f'{p.room.name}')
-        embed.add_embed_field(name='Name', value=f'{p.username}')
-        embed.add_embed_field(name='Message', value=f'{message}',)
-        embed.add_embed_field(name=f'Players In {self.server.config.name}', value=f"{await p.server.redis.hget('houdini.population',f'{self.server.config.id}', encoding='utf-8')}")
-
-
-        # add embed object to webhook
-        webhook.add_embed(embed)
-        time.sleep(1)
-        response = webhook.execute()
+            await webhook.send(embed=embed)
